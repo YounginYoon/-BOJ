8e02c2f5c187406f53eda762b738b371b24d6db4
YounginYoon <3ylsjlsj@gmail.com>
2024-02-03 18:34:18 +0900

    [Gold I] Title: K번째 수, Time: 1120 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -3,10 +3,13 @@ from sys import stdin
 n = int(stdin.readline())
 k = int(stdin.readline())
 
-# arr = []
-# for i in range(1, n + 1):
-#     for j in range(1, n + 1):
-#         arr.append(i * j)
+'''
+1. 1부터 n*n까지 mid를 설정하면서 이분탐색을 한다.
+2. 2차원 배열로 봤을 때, i행의 수들은 i의 배수이다.
+3. 즉, mid보다 작은 수의 개수 == (mid / i)
+4. count >= k 이면, 더 작은 수의 mid를 찾기 위해 end를 줄이고
+    count < k 이면, 더 큰 수의 mid를 찾기 위해 left를 늘인다.
+'''
 
 left = 1
 right = n * n
