f3fbd547bcd3237a20d14423b9f1c722a9e5d4ac
Youngin Yoon <3ylsjlsj@gmail.com>
2024-04-08 21:55:08 +0900

    [Gold IV] Title: 그리드 네트워크, Time: 1036 ms, Memory: 12228 KB, Score: 13 point -BaekjoonHub

--- 백준/Gold/18769. 그리드 네트워크/그리드 네트워크.cc
+++ 백준/Gold/18769. 그리드 네트워크/그리드 네트워크.cc
@@ -4,14 +4,27 @@
 
 using namespace std;
 
-int parent[250000];
+typedef struct _info {
+    int start, end, cost;
+} Info;
 
-void init_parent() {
-    for(int p=0; p<250000; p++) {
+int parent[250000];
+vector<int> ans;
+vector<Info> edge;
+int answer = 0;
+int r, c;
+
+void init() {
+    for(int p=0; p < (r*c); p++) {
             parent[p] = p;
     }
+    answer = 0;
+    edge.clear();
 }
 
+bool compare(Info a, Info b) {
+    return a.cost < b.cost;
+}
 
 int find_parent(int x) {
     if (x == parent[x]) {
@@ -29,15 +42,11 @@ void union_parent(int x, int y) {
 
 int main() {
     int t;
-    vector<int> ans;
 
     cin >> t;
     for(int i=0;i<t;i++){
-        int r, c;
-        vector<vector<int>> edge;
-
         cin >> r >> c;
-        init_parent();
+        init();
         for(int row=0; row<r; row++) {
             for(int col=0; col<c-1;col++) {
                 int start, end, cost;
@@ -45,11 +54,7 @@ int main() {
 
                 start = row * c + col;
                 end = start + 1;
-                vector<int> tmp;
-                tmp.push_back(cost);
-                tmp.push_back(start);
-                tmp.push_back(end);
-                edge.push_back(tmp);
+                edge.push_back({start, end, cost});
             }
         }
 
@@ -60,17 +65,12 @@ int main() {
 
                 start = row * c + col;
                 end = start + c;
-                vector<int> tmp;
-                tmp.push_back(cost);
-                tmp.push_back(start);
-                tmp.push_back(end);
-                edge.push_back(tmp);
+                edge.push_back({start, end, cost});
             }
         }
-        sort(edge.begin(), edge.end());
-        int answer = 0;
+        sort(edge.begin(), edge.end(), compare);
         for (int e = 0; e < edge.size(); e++) {
-            int cost = edge[e][0], start = edge[e][1], end = edge[e][2];
+            int cost = edge[e].cost, start = edge[e].start, end = edge[e].end;
 
             if (find_parent(start) != find_parent(end)) {
                 union_parent(start, end);
